ca0024fd1a2e07457eceaf8c77e9c11874578c55
Dara Heng <daragitgud@gmail.com>
2021-01-11 21:47:18 +1000

    chore: add typescript to homepage and update gitignore

--- src/view/screens/home.tsx
+++ src/view/screens/home.tsx
@@ -1,9 +1,16 @@
-function HomePage ({ children }) {
+import React, { ReactChild } from 'react';
+
+interface HomepageProps {
+  children: ReactChild
+}
+
+function HomePage ({ children }: HomepageProps) {
   return (
     <div>
       <h1>Da Portfolio</h1>
       <p>Hey there, I'm glad you made it here.</p>
       <p>Not much have been added here yet, but they are coming.. 🚀</p>
+      {children}
     </div>
   );
 };
