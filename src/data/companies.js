1efd63f0b2a5e4dea3102ba87a673f7554e5e5b2
darah3ng <daragitgud@gmail.com>
2022-01-20 21:18:24 +1000

    feat: add companies data

---
+++ src/data/companies.js
@@ -0,0 +1,24 @@
+
+import CSIROLogo from '../assets/images/csiro.svg';
+import RexlabsLogo from '../assets/images/rexlabs.svg';
+
+const companies = [
+  {
+    title: 'Rexlabs',
+    alt: 'RX image',
+    role: 'Software Developer',
+    skills: 'Typescript, Javascript, React, Laravel, PHP',
+    period: '2018 - 2021',
+    logo: RexlabsLogo
+  },
+  {
+    title: 'CSIRO',
+    alt: 'CSIRO image',
+    role: 'Industrial Trainee',
+    skills: 'Java, MySQL, PostgreSQL',
+    period: '2017 - 2017',
+    logo: CSIROLogo
+  }
+];
+
+export default companies;
