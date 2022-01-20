1f4ee3d93ab21687add5b9c223b776963147105c
darah3ng <daragitgud@gmail.com>
2022-01-20 21:17:51 +1000

    feat: add gif to project page

---
+++ src/components/screens/Projects.tsx
@@ -0,0 +1,9 @@
+function ProjectsPage() {
+  return (
+    <div>
+      <img src="https://media.giphy.com/media/3o72FkiKGMGauydfyg/giphy.gif" alt="coming soon gif" />
+    </div>
+  );
+};
+
+export default ProjectsPage;
