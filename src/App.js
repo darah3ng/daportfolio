65775105c3b8240692b40e68a20b994f0fa3e2d6
darah3ng <daragitgud@gmail.com>
2024-03-10 15:28:41 +1000

    feat: improve SEO

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import MainLayout from './components/layouts/MainLayout';
-
-function App() {
-  return <MainLayout />;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Helmet } from 'react-helmet';
+import MainLayout from './components/layouts/MainLayout';
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>Dara.</title>
+        <meta name="description" content="Dara Heng - Software Engineer - Blockchain" />
+        <meta name="keywords" content="Dara Heng, Software Engineer, Blockchain Developer, Web Application Development, Innovative Solutions, Creative Coding, Secure Applications, Efficient Problem Solving, Cutting-edge Technologies, Digital Innovation" />
+        <link rel="canonical" href="http://www.daraheng.com" />
+        <meta property="og:title" content="Dara Heng Portfolio Website" />
+        <meta property="og:url" content="http://www.daraheng.com" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
+      <MainLayout />
+    </>
+  );
+}
+
+export default App;
