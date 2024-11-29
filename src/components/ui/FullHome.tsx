4f6f347c6d279aa080f5bd65c78ea94b0afb6dd7
Dara Heng <daragitgud@gmail.com>
2024-11-29 23:06:54 +1000

    feat: add flip words

--- src/components/ui/FullHome.tsx
+++ src/components/ui/FullHome.tsx
@@ -1,7 +1,18 @@
-import { Stack, Heading, Text, useColorMode, VStack, HStack, Link, Box, Image } from '@chakra-ui/react';
+import {
+  Stack,
+  Heading,
+  Text,
+  useColorMode,
+  VStack,
+  HStack,
+  Link,
+  Box,
+  Image,
+} from '@chakra-ui/react';
 import ComponentLayout from '../layouts/ComponentLayout';
 import ScrollingText from '../ui/ScrollingText';
 import Boop from '../animations/Boop.jsx';
+import { FlipWords } from '../ui-aceternity/flip-words';
 
 import Flatpalm from '../../assets/images/flatpalm.png';
 
@@ -15,12 +26,12 @@ const CustomLink = ({ text, href }) => {
     width: '100%',
     height: '4px',
     zIndex: -1,
-    transition: 'all .3s ease-in-out'
+    transition: 'all .3s ease-in-out',
   };
 
   const beforeHoverStyle = {
     textDecoration: 'none',
-    _before: { bottom: 0, height: '100%' }
+    _before: { bottom: 0, height: '100%' },
   };
 
   return (
@@ -41,7 +52,7 @@ function FullHome() {
   const { colorMode } = useColorMode();
 
   return (
-    <ComponentLayout title='Full home page'>
+    <ComponentLayout title="Full home page">
       <VStack
         as="main"
         color="#2D3748"
@@ -54,23 +65,39 @@ function FullHome() {
       >
         <Stack spacing={5}>
           <Box>
-            <HStack verticalAlign='middle'>
+            <HStack verticalAlign="middle">
               <Heading color={`mode.${colorMode}.text`} display="flex">
                 Howdy, I'm Dara
               </Heading>
               <Boop rotation={30} timing={200} scale={1.0}>
-                <Image src={Flatpalm} width='100px' height='100px' transform={'rotate(-25deg)'} />
+                <Image
+                  src={Flatpalm}
+                  width="100px"
+                  height="100px"
+                  transform={'rotate(-25deg)'}
+                />
               </Boop>
-          </HStack>
+            </HStack>
           </Box>
           <Text color={`mode.${colorMode}.subtext`}>
             Your local friendly software dev, and a blockchain lover.
           </Text>
-          <Text color={`mode.${colorMode}.subtext`} fontWeight='bold'>
-            Used to just <CustomLink href='https://academy.binance.com/en/glossary/hodl' text='HODL' /> - now I <CustomLink href='https://academy.binance.com/en/glossary/buidl' text='BUIDL' />.
+          <Text color={`mode.${colorMode}.subtext`} fontWeight="bold">
+            Used to just{' '}
+            <CustomLink
+              href="https://academy.binance.com/en/glossary/hodl"
+              text="HODL"
+            />{' '}
+            - now I{' '}
+            <CustomLink
+              href="https://academy.binance.com/en/glossary/buidl"
+              text="BUIDL"
+            />
+            .
           </Text>
           <Text color={`mode.${colorMode}.subtext`}>
-            This is my digital studio, where I present the things I've worked on and share what I've learned.
+            This is my digital studio, where I present the things I've worked on
+            and share what I've learned.
           </Text>
         </Stack>
 
@@ -92,13 +119,16 @@ function FullHome() {
           </Stack>
         </Stack>
 
-        <HStack w='100%' spacing={5}>
-          <Heading color={`mode.${colorMode}.career.subtext`} fontSize={['sm', 'md', '3xl', '4xl']}>
+        <HStack w="100%" spacing={5}>
+          <Heading
+            color={`mode.${colorMode}.career.subtext`}
+            fontSize={['sm', 'md', '3xl', '4xl']}
+          >
             Check out my{' '}
             <Link
               borderBottom={'2px'}
               pb={2}
-              href='/projects'
+              href="/projects"
               _hover={{ textDecoration: 'none', color: '#fc4a1a' }}
               textDecoration={'none'}
             >
@@ -106,12 +136,23 @@ function FullHome() {
             </Link>
           </Heading>
           <Box>
-            <ScrollingText />
+            <FlipWords
+              className="text-lg"
+              words={[
+                'blockchain',
+                'typescript',
+                'solidity',
+                'ethereum',
+                'foundry',
+                'hardhat',
+              ]}
+              duration={1000}
+            />
           </Box>
         </HStack>
       </VStack>
     </ComponentLayout>
   );
-};
+}
 
 export default FullHome;
