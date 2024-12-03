97ce0833a6bb830bb91f9b0b737d3146aa33c460
Dara <daragitgud@gmail.com>
2024-12-03 12:30:22 +1000

    Feat: UI improvement (#12)
    * feat: UI improvement
    
    * feat: add text for people to say hello

--- src/components/screens/Projects.tsx
+++ src/components/screens/Projects.tsx
@@ -104,7 +104,17 @@ function ProjectsPage() {
                           <MagicButton>Link to app</MagicButton>
                         </Link>
 
-                        <Image src={data.image} />
+                        <motion.div
+                          whileHover={{ scale: 1.05 }}
+                          whileTap={{ scale: 0.95 }}
+                          transition={{
+                            type: 'spring',
+                            stiffness: 400,
+                            damping: 17,
+                          }}
+                        >
+                          <Image src={data.image} />
+                        </motion.div>
                       </CardContent>
                     </Card>
                   </div>
