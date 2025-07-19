d61f7a3a7ed5412da222ce4bdff45ac5d839b6ea
Dara <daragitgud@gmail.com>
2025-07-19 15:25:34 +0100

    feat: add vechain (#13)

--- src/data/companies.js
+++ src/data/companies.js
@@ -2,14 +2,23 @@ import CSIROLogo from '../assets/images/csiro.svg';
 import RexlabsLogo from '../assets/images/rexlabs.svg';
 import Astronaut from '../assets/images/astronaut.svg';
 import Serenade from '../assets/images/Serenade_500x500px.jpeg';
+import VeChain from '../assets/images/vechain.png';
 
 const companies = [
+  {
+    title: 'VeChain',
+    alt: 'VC image',
+    role: 'Software / Blockchain Developer',
+    skills: 'TypeScript, Solidity, Hardhat, Solidity, VeChain Protocol',
+    period: '2025 - Present',
+    logo: VeChain,
+  },
   {
     title: 'Serenade',
     alt: 'SR image',
     role: 'Software / Blockchain Developer',
     skills: 'TypeScript, Go, Next JS, Nest JS, Polygon, Solidity, Hardhat',
-    period: '2022 - Present',
+    period: '2022 - 2024',
     logo: Serenade,
   },
   {
